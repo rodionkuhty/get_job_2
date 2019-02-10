1d6c94a3fd3c2d8e0ae29506ec411ee0fc06c44e
Rodion Kukhtsiy <rodionkuhty@gmail.com>
2019-02-11 02:16:11 +0300

    add some articles

--- src/routes/pages/index.js
+++ src/routes/pages/index.js
@@ -17,31 +17,83 @@ const StyledCard = styled.div`
 `;
 
 class MainPage extends Component {
-  constructor(props) {
-    super(props);
-  }
   render() {
     return (
       <div>
         <h4>Продажа квартив</h4>
         <StyledCard>
-          <Card className="col-sm-2 col-md-6 col-lg-12">
-            <CardImg
-              top
-              width="100%"
-              src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-              alt="Card image cap"
-            />
-            <CardBody>
-              <CardTitle>Card title</CardTitle>
-              <CardSubtitle>Card subtitle</CardSubtitle>
-              <CardText>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </CardText>
-              <Button>Button</Button>
-            </CardBody>
-          </Card>
+          <div className="container-fluid">
+            <div className="row">
+              <Card className=" col-xs-12 col-sm-6 col-md-4 col-lg-2 mr-2">
+                <CardImg
+                  top
+                  width="100%"
+                  src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
+                  alt="Card image cap"
+                />
+                <CardBody>
+                  <CardTitle>Card title</CardTitle>
+                  <CardSubtitle>Card subtitle</CardSubtitle>
+                  <CardText>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </CardText>
+                  <Button>Button</Button>
+                </CardBody>
+              </Card>
+              <Card className=" col-xs-12 col-sm-6 col-md-4 col-lg-2 mr-2">
+                <CardImg
+                  top
+                  width="100%"
+                  src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
+                  alt="Card image cap"
+                />
+                <CardBody>
+                  <CardTitle>Card title</CardTitle>
+                  <CardSubtitle>Card subtitle</CardSubtitle>
+                  <CardText>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </CardText>
+                  <Button>Button</Button>
+                </CardBody>
+              </Card>
+              <Card className=" col-xs-12 col-sm-6 col-md-4 col-lg-2 mr-2">
+                <CardImg
+                  top
+                  width="100%"
+                  src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
+                  alt="Card image cap"
+                />
+                <CardBody>
+                  <CardTitle>Card title</CardTitle>
+                  <CardSubtitle>Card subtitle</CardSubtitle>
+                  <CardText>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </CardText>
+                  <Button>Button</Button>
+                </CardBody>
+              </Card>
+              <Card className=" col-xs-12 col-sm-6 col-md-4 col-lg-2 mr-2">
+                <CardImg
+                  top
+                  width="100%"
+                  src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
+                  alt="Card image cap"
+                />
+                <CardBody>
+                  <CardTitle>Card title</CardTitle>
+                  <CardSubtitle>Card subtitle</CardSubtitle>
+                  <CardText>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </CardText>
+                  <Button>Button</Button>
+                </CardBody>
+              </Card>
+            </div>
+          </div>
         </StyledCard>
       </div>
     );
